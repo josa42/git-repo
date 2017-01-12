c794daad3e9dad762b14cd21e7e844994181c2fe
Josa Gesell <josa.gesell@ligadigital.com>
2017-01-12 21:16:47 +0100

    Release 0.3.0

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 		  --version          Show version.
   `)
 
-	arguments, _ := docopt.Parse(usage, nil, true, "Git Release 0.1.0", false)
+	arguments, _ := docopt.Parse(usage, nil, true, "Git Release 0.3.0", false)
 
 	repo := utils.GetRepo()
 
