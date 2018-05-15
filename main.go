813318faa0dd138472fa96f8cd70a6fd8160ef5d
Josa Gesell <josa@gesell.me>
2018-05-15 11:32:11 +0200

    🎉  Release 0.5.6

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 		  --version          Show version.
   `)
 
-	arguments, _ := docopt.Parse(usage, nil, true, "Git Release 0.5.5", false)
+	arguments, _ := docopt.Parse(usage, nil, true, "Git Release 0.5.6", false)
 
 	repo := utils.GetRepo()
 
